cf8dada614bc5bef9009596689a1b415d2b20af5
Keelan002 <osullivankeelan@gmail.com>
2024-02-19 11:04:59 +0000

    cretaed gallery screen to select which category of images the user wants to view

---
+++ app/src/main/java/mtu/research_project/researchprojectapp/Screens/GalleryScreen.kt
@@ -0,0 +1,121 @@
+package mtu.research_project.researchprojectapp.Screens
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import mtu.research_project.researchprojectapp.Theme.primaryColor
+import mtu.research_project.researchprojectapp.Theme.secondaryColor
+import mtu.research_project.researchprojectapp.ViewModel.AppViewModel
+
+@Composable
+fun GalleryScreen(navHController: NavHostController, appViewModel: AppViewModel){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        GalleryScreenContent(navHController = navHController, appViewModel)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun GalleryScreenContent(navHController: NavHostController, appViewModel: AppViewModel){
+    var selectedTabIndex by remember { mutableStateOf(0) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "App name") },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = secondaryColor),
+            )
+        },
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 48.dp)
+                    .background(primaryColor)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    PrimaryTabRowDemo(
+                        selectedTabIndex = selectedTabIndex,
+                        onTabSelected = { tabIndex ->
+                            selectedTabIndex = tabIndex
+                        },
+                        navController = navHController
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 20.dp, end = 20.dp, top = 30.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    // First Box
+                    Box(
+                        modifier = Modifier
+                            .clickable {
+                                navHController.navigate(Screens.ImageScreen.route)
+                            }
+                            .weight(1f)
+                            .height(100.dp)
+                            .background(secondaryColor)
+                            .padding(16.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "Drink", color = Color.White)
+                    }
+
+                    Spacer(modifier = Modifier.width(30.dp))
+
+                    // Second Box
+                    Box(
+                        modifier = Modifier
+                            .clickable {
+                                navHController.navigate(Screens.ImageScreen.route)
+                            }
+                            .weight(1f)
+                            .height(100.dp)
+                            .background(secondaryColor)
+                            .padding(16.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "Food", color = Color.White)
+                    }
+                }
+
+            }
+        }
+    )
+}
