9d24d115ded358081bc5ec71e8637d335708776b
Keelan002 <osullivankeelan@gmail.com>
2024-02-19 10:32:34 +0000

    initial commit

---
+++ app/src/main/java/mtu/research_project/researchprojectapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package mtu.research_project.researchprojectapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
