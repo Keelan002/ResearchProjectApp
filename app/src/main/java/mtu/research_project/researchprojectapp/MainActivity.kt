e2ff69bc4c65e685b358dab8d74ce67114d7e4b1
Keelan002 <osullivankeelan@gmail.com>
2024-02-19 11:16:32 +0000

    updated MainActivity.kt to display screens and allow for navigation

--- app/src/main/java/mtu/research_project/researchprojectapp/MainActivity.kt
+++ app/src/main/java/mtu/research_project/researchprojectapp/MainActivity.kt
@@ -2,10 +2,87 @@ package mtu.research_project.researchprojectapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import mtu.research_project.researchprojectapp.Screens.CameraScreen
+import mtu.research_project.researchprojectapp.Screens.CaptureScreen
+import mtu.research_project.researchprojectapp.Screens.CheckPermissionScreen
+import mtu.research_project.researchprojectapp.Screens.GalleryScreen
+import mtu.research_project.researchprojectapp.Screens.ImageScreen
+import mtu.research_project.researchprojectapp.Screens.Screens
+import mtu.research_project.researchprojectapp.ViewModel.AppViewModel
+import mtu.research_project.researchprojectapp.ViewModel.CameraViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appViewModel: AppViewModel
+    private val cameraViewModel: CameraViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        appViewModel = ViewModelProvider(this).get(AppViewModel::class.java)
+        setContent {
+            Surface (
+                modifier = Modifier.fillMaxSize(),
+            ){
+                DemoApp(cameraViewModel)
+            }
+        }
+    }
+
+    @Composable
+    fun DemoApp(cameraViewModel: CameraViewModel){
+        val navController = rememberNavController()
+        Scaffold { innerPadding ->
+            NavHost(
+                navController,
+                modifier = Modifier.padding(innerPadding),
+                cameraViewModel = cameraViewModel,
+                appViewModel = appViewModel
+            )
+        }
+    }
+
+    @Composable
+    fun NavHost(
+        navController: NavHostController,
+        modifier: Modifier = Modifier,
+        cameraViewModel: CameraViewModel,
+        appViewModel: AppViewModel
+    ) {
+
+
+        androidx.navigation.compose.NavHost(
+            navController = navController,
+            startDestination = Screens.CaptureScreen.route,
+            modifier = modifier
+        ) {
+            composable(route = Screens.CaptureScreen.route) {
+                CaptureScreen(navController, appViewModel)
+            }
+            composable(route = Screens.GalleryScreen.route) {
+                GalleryScreen(navController, appViewModel)
+            }
+            composable(route = Screens.ImageScreen.route) {
+                ImageScreen(navController, appViewModel)
+            }
+            composable(route = Screens.CameraScreen.route) {
+                CameraScreen(cameraViewModel, appViewModel, cameraViewModel, navController)
+            }
+            composable(route = Screens.MainCameraScreen.route) {
+                CheckPermissionScreen(cameraViewModel, appViewModel, navController)
+
+            }
+        }
     }
 }
